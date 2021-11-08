4659cb0efface85dd17327eac7c48d28a25f4ff5
nadiaabug <93927813+nadiaabug@users.noreply.github.com>
2021-11-08 16:23:02 +0200

    Add files via upload

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField] float _launchforce = 500;
+    Vector2 _startPosition;
+    Rigidbody2D _rigidbody2D;
+    SpriteRenderer _spriteRenderer;
+
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPosition = _rigidbody2D.position;
+        _rigidbody2D.isKinematic = true;
+    }
+    void OnMouseDown()
+    {
+        _spriteRenderer.color = Color.red;
+    }
+    void OnMouseUp()
+    {
+        Vector2 currentposition = _rigidbody2D.position;
+        Vector2 direction = _startPosition - currentposition;
+        direction.Normalize();
+        _rigidbody2D.isKinematic = false;
+        _rigidbody2D.AddForce(direction * _launchforce);
+        _spriteRenderer.color = Color.white;
+    }
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+       StartCoroutine(ResetAfterDelay());
+    }
+    IEnumerator ResetAfterDelay()
+    {
+        yield return new WaitForSeconds(3);
+        _rigidbody2D.position = _startPosition;
+        _rigidbody2D.isKinematic = true;
+        _rigidbody2D.velocity = Vector2.zero;
+    }
+}
+
---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [SerializeField] Sprite _deadSprite;
+    [SerializeField] ParticleSystem _particleSystem;
+    bool _hasDied;
+    
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(ShouldDieFromCollision(collision))
+        {
+            Die();
+        }
+    }
+    bool ShouldDieFromCollision(Collision2D collision)
+    {
+        Bird bird = collision.gameObject.GetComponent<Bird>();
+        if (bird != null)
+            return true;
+        if (collision.contacts[0].normal.y < -0.5)
+            return true;
+        return false;
+    }
+    
+    void Die()
+    {
+        _hasDied = true;
+        GetComponent<SpriteRenderer>().sprite = _deadSprite;
+        _particleSystem.Play();
+    }
+
+}
